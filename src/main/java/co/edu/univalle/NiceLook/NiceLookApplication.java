deb661cbbcc832fe4832c52afde17d8884808d39
sebrivdeveloper-design <sebrivdeveloper@gmail.com>
2026-03-20 18:28:57 -0500

    Proyecto Spring boot Inicial

---
+++ src/main/java/co/edu/univalle/NiceLook/NiceLookApplication.java
@@ -0,0 +1,13 @@
+package co.edu.univalle.NiceLook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceLookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceLookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/univalle/NiceLook/NiceLookApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.univalle.NiceLook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceLookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
